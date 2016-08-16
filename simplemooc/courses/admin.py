99997eaf02726cba0fa3ea6923aa8e35b4fd7126
Wellington Moraes <wellpunk@gmail.com>
2016-08-15 21:24:23 -0300

    Create admin app on courses

--- simplemooc/courses/admin.py
+++ simplemooc/courses/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Course
 
-# Register your models here.
+
+class CourseAdmin(admin.ModelAdmin):
+
+    list_display = ['name', 'slug', 'start_date', 'created_at']
+    search_fields = ['name', 'slug']
+
+admin.site.register(Course, CourseAdmin)
