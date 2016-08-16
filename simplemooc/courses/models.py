d7805ec6683297aac57adf610d5a5a3cae104099
Wellington Moraes <wellpunk@gmail.com>
2016-08-15 21:24:56 -0300

    Add object search

--- simplemooc/courses/models.py
+++ simplemooc/courses/models.py
@@ -2,6 +2,15 @@
 from django.db import models
 
 
+class CourseManager(models.Manager):
+
+    def search(self, query):
+        return self.get_queryset().filter(
+            models.Q(name__icontains=query) |
+            models.Q(description__icontains=query)
+        )
+
+
 class Course(models.Model):
     """Courses."""
 
@@ -19,3 +28,13 @@ class Course(models.Model):
         'Criado em', auto_now_add=True
     )
     updated_at = models.DateTimeField('Atualizado em', auto_now=True)
+
+    objects = CourseManager()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Curso'
+        verbose_name_plural = 'Cursos'
+        ordering = ['name']
